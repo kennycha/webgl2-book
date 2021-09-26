76dc8f6a7ad9fd60aa69ee30f3a6e7906b4d8377
kennycha <dudqn136@naver.com>
2021-09-26 17:22:45 +0900

    feat: add examples using Goraud shading with Lambertian light
    - one with static light direction
    - the other with light direction rotating

--- common/js/utils.js
+++ common/js/utils.js
@@ -56,6 +56,63 @@ const utils = {
     return shader;
   },
 
+  calculateNormals(vs, ind) {
+    const x = 0;
+    const y = 1;
+    const z = 2;
+    const ns = [];
+
+    for (let i = 0; i < vs.length; i += 3) {
+      ns[i + x] = 0.0;
+      ns[i + y] = 0.0;
+      ns[i + z] = 0.0;
+    }
+
+    for (let i = 0; i < ind.length; i += 3) {
+      const v1 = [];
+      const v2 = [];
+      const normals = [];
+
+      v1[x] = vs[3 * ind[i + 2] + x] - vs[3 * ind[i + 1] + x];
+      v1[y] = vs[3 * ind[i + 2] + y] - vs[3 * ind[i + 1] + y];
+      v1[z] = vs[3 * ind[i + 2] + z] - vs[3 * ind[i + 1] + z];
+
+      v2[x] = vs[3 * ind[i] + x] - vs[3 * ind[i + 1] + x];
+      v2[y] = vs[3 * ind[i] + y] - vs[3 * ind[i + 1] + y];
+      v2[z] = vs[3 * ind[i] + z] - vs[3 * ind[i + 1] + z];
+
+      normals[x] = v1[x] * v2[z] - v1[z] * v2[y];
+      normals[y] = v1[z] * v2[x] - v1[x] * v2[z];
+      normals[z] = v1[x] * v2[y] - v1[y] * v2[x];
+
+      for (let j = 0; j < 3; j += 1) {
+        ns[3 * ind[i + j] + x] = ns[3 * ind[i + j] + x] + normals[x];
+        ns[3 * ind[i + j] + y] = ns[3 * ind[i + j] + y] + normals[y];
+        ns[3 * ind[i + j] + z] = ns[3 * ind[i + j] + z] + normals[z];
+      }
+    }
+
+    for (let i = 0; i < vs.length; i += 3) {
+      const nn = [];
+      nn[x] = ns[i + x];
+      nn[y] = ns[i + y];
+      nn[z] = ns[i + z];
+
+      let len = Math.sqrt(nn[x] * nn[x] + nn[y] * nn[y] + nn[z] * nn[z]);
+      if (len === 0) len = 1.0;
+
+      nn[x] = nn[x] / len;
+      nn[y] = nn[y] / len;
+      nn[z] = nn[z] / len;
+
+      ns[i + x] = nn[x];
+      ns[i + y] = nn[y];
+      ns[i + z] = nn[z];
+    }
+
+    return ns;
+  },
+
   configureControls(settings, options = { width: 300 }) {
     const gui = options.gui || new dat.GUI(options);
     const state = {};
